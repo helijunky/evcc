32168e2cf51f412fa399cd93a023ce51bd6f2c57
Manuel Haag <manux@gmx.net>
2026-07-01 20:03:06 +0200

    Disable charger and set current to 6A after plugin to get into normal wait state

--- charger/zaptec.go
+++ charger/zaptec.go
@@ -204,6 +204,13 @@ func (c *Zaptec) Status() (api.ChargeStatus, error) {
 	if err == nil && currentStatus == zaptec.OpModeDisconnected && currentStatus != c.lastStatus {
 		err = c.MaxCurrentMillis(0)
 	}
+	if err == nil && currentStatus == zaptec.OpModeConnectedRequesting && currentStatus != c.lastStatus {
+		current, err := res.ObservationByID(zaptec.ChargerMaxCurrent).Float64()
+		if err == nil && current == 0 {
+			err = c.Enable(false)
+			err = c.MaxCurrentMillis(6)
+		}
+	}
 	c.lastStatus = currentStatus
 
 	switch currentStatus {
