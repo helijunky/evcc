0d24fbe02261b21d06b6c2d62ad8fcc1a7e233c2
Manuel Haag <manux@gmx.net>
2026-07-01 21:59:00 +0200

    set into stopped mode whenever the current is below the minimum threshold of 6A

--- charger/zaptec.go
+++ charger/zaptec.go
@@ -206,7 +206,7 @@ func (c *Zaptec) Status() (api.ChargeStatus, error) {
 	}
 	if err == nil && currentStatus == zaptec.OpModeConnectedRequesting && currentStatus != c.lastStatus {
 		current, err := res.ObservationByID(zaptec.ChargerMaxCurrent).Float64()
-		if err == nil && current == 0 {
+		if err == nil && current < 6 {
 			err = c.Enable(false)
 			err = c.MaxCurrentMillis(6)
 		}
